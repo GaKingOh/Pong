9964a6a3910b0627307f469637fb43ad98e406fc
GaKingOh <oh20020409@gmail.com>
2025-07-28 20:03:30 +0900

    충돌 소리:

--- Assets/ballController.cs
+++ Assets/ballController.cs
@@ -7,16 +7,19 @@ public class ballController : MonoBehaviour
     Rigidbody2D rb;
     GameObject gameDirector;
     int[] direction = { -1, 1 };
+    AudioSource ad;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
         rb.AddForce(new Vector2(100f * direction[Random.Range(0, 2)], Random.Range(80, 100)) * 2);
         gameDirector = GameObject.Find("gameDirector");
- 
+        ad = GetComponent<AudioSource>();
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
+        ad.Play();
         if (rb.velocity.magnitude > 15) return; // 공의 최대 속도
         rb.velocity *= 1.05f;
     }
@@ -31,7 +34,7 @@ public class ballController : MonoBehaviour
     {
         while (rb.velocity.magnitude < 3 && rb.velocity.magnitude != 0)
         {
-            rb.velocity += rb.velocity * 0.5f;
+            rb.velocity += rb.velocity.normalized * 0.5f;
         }
         if (transform.position.x < -10 || transform.position.x > 10) // -10~10 까지 공이 이동할 수 있는 범위
         {
